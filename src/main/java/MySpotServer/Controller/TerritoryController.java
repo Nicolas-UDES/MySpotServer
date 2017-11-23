017d5f791536ca13f7deba5146f89c9319e5ef3a
DESKTOP-7AIVUAH\Squirrel <lamn2005@usherbrooke.ca>
2017-11-23 14:18:17 -0500

    Can now access the territories from the server.

--- src/main/java/MySpotServer/Controller/TerritoryController.java
+++ src/main/java/MySpotServer/Controller/TerritoryController.java
@@ -6,16 +6,22 @@ import MySpotServer.Utility.JsonToTerritories;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
-@Controller("/mapCreation")
-public class MapCreationController {
+@Controller("/territory")
+public class TerritoryController {
 
-	@GetMapping("/readJson")
-	public @ResponseBody String greeting() throws Exception {
+	@PostMapping("/readJson")
+	public @ResponseBody String readJson() throws Exception {
 		JsonToTerritories.readJson();
+		return "Done!";
+	}
+
+	@GetMapping("/getTerritories")
+	public @ResponseBody String getTerritories() throws Exception {
 		List<MySpotServer.Entites.Territory> result = TerritoryDAO.GetAllTerritories();
 		return new ObjectMapper().writeValueAsString(result);
 	}
--- src/main/java/MySpotServer/Utility/JsonToTerritories.java
+++ src/main/java/MySpotServer/Utility/JsonToTerritories.java
@@ -23,25 +23,64 @@ public class JsonToTerritories {
 			Territory territory = new Territory();
 			for(ArrayList<ArrayList<Double>> polygon : feature.getGeometry().getCoordinates()) {
 				for(ArrayList<Double> point : polygon) {
-					territory.getPositions().add(new LatLng(point.get(0), point.get(1)));
+					territory.getPositions().add(new LatLng((float)(double)point.get(1), (float)(double)point.get(0)));
 				}
 			}
 			territories.add(territory);
 		}
 
-		mergeTerritories(territories);
+		mergeTerritories(territories, 10.0);
+		cleanDuplicates(territories);
 		TerritoryDAO.AddTerritories(territories);
 	}
 
-	public static void mergeTerritories(List<Territory> territories) {
-		for(Territory territory : territories) {
-			for(LatLng latLng : territory.getPositions()) {
-				for(Territory subTerritory : territories) {
-					for(LatLng subLatLng : subTerritory.getPositions()) {
-						if(latLng.distance(subLatLng) < 1.0) {
-							subLatLng.setLat(latLng.getLat());
-							subLatLng.setLng(latLng.getLng());
+	/**
+	 * Merges points which's distance is bellow the one requested.
+	 * @param territories
+	 * @param minDistanceInMeter
+	 */
+	public static void mergeTerritories(List<Territory> territories, double minDistanceInMeter) {
+		for (int a1 = 0; a1 < territories.size(); a1++) {
+			List<LatLng> positions1 = territories.get(a1).getPositions();
+
+			for (int b1 = 0; b1 < positions1.size(); b1++) {
+				LatLng latLng1 = positions1.get(b1);
+
+				for (int a2 = a1; a2 < territories.size(); a2++) {
+					List<LatLng> positions2 = territories.get(a2).getPositions();
+
+					for (int b2 = 0; b2 < positions2.size(); b2++) {
+						LatLng latLng2 = positions2.get(b2);
+
+						if (latLng1.distance(latLng2) < minDistanceInMeter) {
+							latLng2.setLatitude(latLng1.getLatitude());
+							latLng2.setLongitude(latLng1.getLongitude());
 						}
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * Removes useless points within a same territory.
+	 * @param territories
+	 */
+	public static void cleanDuplicates(List<Territory> territories) {
+		for (Territory territory : territories) {
+			List<LatLng> positions1 = territory.getPositions();
+			for (int a = 0; a < positions1.size(); a++) {
+				List<LatLng> positions2 = territory.getPositions();
+				for (int b = 0; b < positions2.size(); b++) {
+					if(a == b) {
+						continue;
+					}
+
+					LatLng latLng1 = positions1.get(a);
+					LatLng latLng2 = positions2.get(b);
+					if(latLng1.equals(latLng2)) {
+						positions2.remove(b);
+						--b;
 					}
 				}
 			}
